e68ca6c45e27206b307614df525a2d2f01d16e29
Manish Bhusal <84217955+manish-bhusal@users.noreply.github.com>
2022-10-29 11:09:25 +0545

    Add files via upload

---
+++ data.py
@@ -0,0 +1,102 @@
+question_data = [
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The Xenomorph from the science fiction film &quot;Alien&quot; has acidic blood.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "In Undertale, having a &quot;Fun Value&quot; set to 56-57 will play the &quot;Wrong Number Song Call&quot;.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+                "True"
+        ]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "TF2: The Medic will be credited for an assist if he heals a Spy that successfully saps a building.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Japanese Anime & Manga",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In the &quot;Melancholy of Haruhi Suzumiya&quot; series, the narrator goes by the nickname Kyon.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    },
+    {
+        "category": "Geography",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "Switzerland has four national languages, English being one of them.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+                "True"
+        ]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Fast food restaurant chains Carl&#039;s Jr. and Hardee&#039;s are owned by the same company.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The movie &quot;Tron&quot; received an Oscar nomination for Best Visual Effects.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+                "True"
+        ]
+    },
+    {
+        "category": "Science & Nature",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Not including false teeth; A human has two sets of teeth in their lifetime.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "In &quot;Resident Evil&quot;, only Chris has access to the grenade launcher.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+                "True"
+        ]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The shotgun appears in every numbered Resident Evil game.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    }
+]
